b6a840c1c745477baa666fd87fae64c779e30163
olegkovtun <olegkovtun@gmail.com>
2024-05-27 15:41:54 +0300

    .

--- src/main/java/com/company/first.java
+++ src/main/java/com/company/first.java
@@ -1,17 +1,22 @@
 package com.company;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class first {
+
+    public static void main(String[] args) {
+        System.out.print("Вкажіть перше число: ");
+        Scanner num1 = new Scanner(System.in);
+        int x = num1.nextInt();
+
+        System.out.print("Вкажіть друге число: ");
+        Scanner num2 = new Scanner(System.in);
+        int y = num2.nextInt();
+
+
+
+        System.out.println(x / y);
+        System.out.println(x % y);
     }
 }
+
